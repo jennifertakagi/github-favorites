50653350dd3cde156f6195f38b9b3357d4ee3929
jennifertakagi <jennitakagi@gmail.com>
2021-02-02 08:06:42 -0300

    feat: add Spinner component and icon

---
+++ web/src/components/Spinner/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import loadingIcon from '../../assets/icons/spinner.gif';
+
+
+function Spinner({ show }) {
+  return (
+    <div style={{display: show ? 'block' : 'none', margin: '20px auto 0', textAlign: 'center'}}   >
+      <img src={loadingIcon} />
+    </div>
+  );
+}
+
+export default Spinner;
