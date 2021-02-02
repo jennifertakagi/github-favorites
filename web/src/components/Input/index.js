af690dbad7323cc7804e2712c32e8d763e64fe7d
jennifertakagi <jennitakagi@gmail.com>
2021-02-02 08:05:55 -0300

    feat: add Input component and style

---
+++ web/src/components/Input/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles.css";
+
+function Input({ id, name, placeholder, onChange, type, value }) {
+  return (
+    <input id={id} name={name} placeholder={placeholder} onChange={onChange} type={ type } value={value} />
+  );
+}
+
+export default Input;
