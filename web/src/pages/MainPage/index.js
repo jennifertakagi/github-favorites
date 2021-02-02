e8120fa328fdb71581da6eefec8ce96128985d9c
jennifertakagi <jennitakagi@gmail.com>
2021-02-02 08:13:59 -0300

    feat: add input on main page, error message and loading

--- web/src/pages/MainPage/index.js
+++ web/src/pages/MainPage/index.js
@@ -1,28 +1,61 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import RepositoryCard from "../../components/RepositoryCard";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import Spinner from "../../components/Spinner";
 
 import "./styles.css";
 import api from "../../services/api"
 
 function MainPage() {
+  const [user, setUser] = useState('');
   const [repositories, setRepositories] = useState([]);
+  const [showLoading, setShowLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  useEffect(() => {
-    api.get('/starred/jennifertakagi')
+  const handleInputChange = useCallback((newUser) => {
+    setUser(newUser)
+    setErrorMessage('')
+  }, [user, errorMessage])
+
+  const handleUserSearch = useCallback(() => {
+    try {
+      setRepositories([])
+      setShowLoading(true);
+
+      api.get(`/starred/${user}`)
       .then(response => {
-        const { data = {} } = response;
-        setRepositories(data.repositories);
-      })
-  }, [])
+        const data = response;
+        const repositories = (data && data.data && data.data.repositories) || [];
+        setRepositories(repositories);
+
+        if (!repositories.length) { setErrorMessage('The user does not exist, try another one') }
+        setShowLoading(false);
+    })
+    } catch (error) {
+      setErrorMessage('An error occurred, try again')
+      setShowLoading(false);
+    }
+  }, [user, showLoading, errorMessage])
 
   return (
     <div id="main-page">
       <h1>Go GitHub App</h1>
-      <ul id="repository-list">
+
+      <div className="search-area">
+        <Input id="userInput" name="userInput" placeholder="Who's the user?" type="text" value={user} onChange={(e) => handleInputChange(e.target.value)}/>
+        <Button type="button" onClick={handleUserSearch}>search</Button>
+      </div>
+      
+      {showLoading && <Spinner show={showLoading} />}
+
+{errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      {repositories && (<ul id="repository-list">
       {repositories.map(repository => {
         return <RepositoryCard repository={ repository } />
       })}
-    </ul>
+    </ul>)}
     </div>
   );
 }
